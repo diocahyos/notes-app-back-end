bf1c2d5834010f8c769eff4cc4c760da4dbd238f
dio.cahyo <dio.cahyo@idstar.co.id>
2024-01-09 11:17:25 +0700

    postgres database implemented

--- src/exceptions/NotFoundError.js
+++ src/exceptions/NotFoundError.js
@@ -2,7 +2,7 @@ const ClientError = require('./ClientError')
 
 class NotFoundError extends ClientError {
   constructor (messgae) {
-    super(messgae)
+    super(messgae, 404)
     this.name = 'NotFoundError'
   }
 }
