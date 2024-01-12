677fa6aa0a877ddbf53c9df8ceb3064263b88607
dio.cahyo <dio.cahyo@idstar.co.id>
2024-01-12 10:10:40 +0700

    user registration implemented

--- migrations/1705025344518_create-table-users.js
+++ migrations/1705025344518_create-table-users.js
@@ -3,34 +3,27 @@
 exports.shorthands = undefined
 
 exports.up = pgm => {
-  pgm.createTable('notes', {
+  pgm.createTable('users', {
     id: {
       type: 'VARCHAR(50)',
       primaryKey: true
     },
-    title: {
+    username: {
+      type: 'VARCHAR(50)',
+      unique: true,
+      notNull: true
+    },
+    password: {
       type: 'TEXT',
       notNull: true
     },
-    body: {
-      type: 'TEXT',
-      notNull: true
-    },
-    tags: {
-      type: 'TEXT[]',
-      notNull: true
-    },
-    created_at: {
-      type: 'TEXT',
-      notNull: true
-    },
-    updated_at: {
+    fullname: {
       type: 'TEXT',
       notNull: true
     }
   })
 }
 
-exports.down = pgm => {
-  pgm.dropTable('notes')
+exports.down = (pgm) => {
+  pgm.dropTable('users')
 }
