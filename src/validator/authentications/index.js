f015afb57e5d7e75b09248640006b27f83a2d482
dio.cahyo <dio.cahyo@idstar.co.id>
2024-01-12 13:29:11 +0700

    authentication implemented

---
+++ src/validator/authentications/index.js
@@ -0,0 +1,25 @@
+const InvariantError = require('../../exceptions/InvariantError')
+const { PostAuthenticationPayloadSchema, PutAuthenticationPayloadSchema, DeleteAuthenticationPayloadSchema } = require('./schema')
+
+const AuthencticationValidator = {
+  validatorPostAuthenticationPayload: (payload) => {
+    const validationResult = PostAuthenticationPayloadSchema.validate(payload)
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message)
+    }
+  },
+  validatorPutAuthenticationPayload: (payload) => {
+    const validationResult = PutAuthenticationPayloadSchema.validate(payload)
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message)
+    }
+  },
+  validatorDeleteAuthenticationPayload: (payload) => {
+    const validationResult = DeleteAuthenticationPayloadSchema.validate(payload)
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message)
+    }
+  }
+}
+
+module.exports = AuthencticationValidator
